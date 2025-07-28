9d17cbf440608cb54454d47fb68adb6bcabe67d2
Michal Hankus <michal83h@gmail.com>
2025-07-28 11:42:34 +0000

    New stage version 1.10.1

--- Formula/bdy-stage.rb
+++ Formula/bdy-stage.rb
@@ -1,9 +1,9 @@
 class BdyStage < Formula
   desc "Buddy cli"
   homepage "https://buddy.works"
-  url "https://es.buddy.works/bdy/stage/1.9.48/darwin-arm64.tar.gz"
-  sha256 "7912f50ac75d70e15c9f099a67bcc37625b3cfbe947a864c0ba3e5481d1be4cc"
-  version "1.9.48"
+  url "https://es.buddy.works/bdy/stage/1.10.1/darwin-arm64.tar.gz"
+  sha256 "fa7f8772bb24ee8c3d337488e689b003538c16880af58027cf8784d1ddd1bf4a"
+  version "1.10.1"
   def install
     bin.install "bdy"
   end
