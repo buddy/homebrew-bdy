9688ddb87326fa0937450713765895e2f493a526
Michal Hankus <michal83h@gmail.com>
2025-12-10 11:45:44 +0000

    New master version 1.16.5

--- Formula/bdy-master.rb
+++ Formula/bdy-master.rb
@@ -1,9 +1,9 @@
 class BdyMaster < Formula
   desc "Buddy cli"
   homepage "https://buddy.works"
-  url "https://es.buddy.works/bdy/master/1.15.6/darwin-arm64.tar.gz"
-  sha256 "66bd84c47781aa27ee2e847e344d1f1389db4f9a8676b26be33d02adf242a2d4"
-  version "1.15.6"
+  url "https://es.buddy.works/bdy/master/1.16.5/darwin-arm64.tar.gz"
+  sha256 "8f69f7c4c832129932417afcd4b473c3b8c28e8dbbd3478ac0a5de0ca2c57683"
+  version "1.16.5"
   def install
     bin.install "bdy"
   end
