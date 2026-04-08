9f5b3c4dd204543ee8c325abade30eeed4b091c4
Michal Hankus <michal83h@gmail.com>
2026-04-08 11:31:02 +0000

    New dev version 1.19.3

--- Formula/bdy-dev.rb
+++ Formula/bdy-dev.rb
@@ -1,9 +1,9 @@
 class BdyDev < Formula
   desc "Buddy cli"
   homepage "https://buddy.works"
-  url "https://es.buddy.works/bdy/dev/1.19.2/darwin-arm64.tar.gz"
-  sha256 "108866f23e113675bc8d8ca76ee1d1896da91ca73425449441a6317669836ad3"
-  version "1.19.2"
+  url "https://es.buddy.works/bdy/dev/1.19.3/darwin-arm64.tar.gz"
+  sha256 "7e7bce2a5e1902e076cca0db79b72288028f5c974834496011a632fc1e95022b"
+  version "1.19.3"
   def install
     bin.install "bdy"
   end
