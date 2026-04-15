1baa86457f0a634c56adaebf1b3acfe401d29bf2
Michal Hankus <michal83h@gmail.com>
2026-04-15 07:48:45 +0000

    New beta version 1.20.2

--- Formula/bdy-beta.rb
+++ Formula/bdy-beta.rb
@@ -1,9 +1,9 @@
 class BdyBeta < Formula
   desc "Buddy cli"
   homepage "https://buddy.works"
-  url "https://es.buddy.works/bdy/beta/1.20.0/darwin-arm64.tar.gz"
-  sha256 "4b901a6099bfae3c6d8f1f68a67ff36665fae8c0cfe653abb4adcd021a65132a"
-  version "1.20.0"
+  url "https://es.buddy.works/bdy/beta/1.20.2/darwin-arm64.tar.gz"
+  sha256 "73aec44fe0aa38532aacc44615bc05494435ef02b570e508e597c141a62f922a"
+  version "1.20.2"
   def install
     bin.install "bdy"
   end
