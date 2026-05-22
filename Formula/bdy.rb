2e2a0237c668069518dad00f1d3254730a7d5c08
Michal Hankus <michal83h@gmail.com>
2026-05-22 05:37:59 +0000

    New prod version 1.22.53

--- Formula/bdy.rb
+++ Formula/bdy.rb
@@ -1,9 +1,9 @@
 class Bdy < Formula
   desc "Buddy cli"
   homepage "https://buddy.works"
-  url "https://es.buddy.works/bdy/prod/1.22.49/darwin-arm64.tar.gz"
-  sha256 "a9842daf3e2aa0808528852bbc7cdbb15af469e7580803176b0ae5cc1466ba24"
-  version "1.22.49"
+  url "https://es.buddy.works/bdy/prod/1.22.53/darwin-arm64.tar.gz"
+  sha256 "22f930878a656ca13192af607c3403e882a90a1b4c2c6a47e429aca8cf10d99a"
+  version "1.22.53"
   def install
     bin.install "bdy"
   end
